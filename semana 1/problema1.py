1abf07924a0807b856a070815454d236af38f754
MalpikCruE28 <86462951+MalpikCruE28@users.noreply.github.com>
2025-02-03 21:44:11 -0600

    semana 1
    semana 1 ejercicios

---
+++ semana 1/problema1.py
@@ -0,0 +1,21 @@
+#calculadora basica 
+
+numA=input("ingrese el numero A:")
+
+numB=input("ingrese el numero B:")
+
+numA = int(numA)
+numB = int(numB)
+
+suma=(numA+numB)
+print (f"la suma es:{suma}")
+
+resta=(numA-numB)
+print (f"la resta es:{resta}")
+
+mul=(numA*numB)
+print (f"la multiplicacion es:{mul}")
+
+
+div=(numA/numB)
+print(f"la division es {div}")
