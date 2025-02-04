0550fbaaa52df8cb019b87642da3b6f98b41ccf9
MalpikCruE28 <86462951+MalpikCruE28@users.noreply.github.com>
2025-02-03 21:46:19 -0600

    Delete problema3.py

--- problema3.py
+++
@@ -1,8 +0,0 @@
-#determinar par o impar
-
-numero=int(input("ingrese un numero para determinar si es par o impar"))
-
-if numero % 2 == 0:
-    print("el numero ingresado es par")
-else:
-    print("el numero ingresado es impar")
